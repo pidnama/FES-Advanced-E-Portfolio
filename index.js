9e9a17422fd2cfa3a0e91fb4551cf22344a1da2c
pidnama <amandips181@gmail.com>
2023-07-12 11:59:42 +1000

    Added modal functionality

---
+++ index.js
@@ -0,0 +1,39 @@
+// TEMPLATE ID: template_co1ocyt
+// Service ID: service_wtl0a9d
+// UID: hLSWPmSsE780AMibL
+
+// note we can also use async/await
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_wtl0a9d",
+      "template_co1ocyt",
+      event.target,
+      "hLSWPmSsE780AMibL"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on amandips181@gmail.com"
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
