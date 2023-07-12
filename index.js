6e9ddc838044326604a2e7392ecf4f7a33a6b61b
pidnama <amandips181@gmail.com>
2023-07-12 13:36:37 +1000

    Added moving shapes

--- index.js
+++ index.js
@@ -2,7 +2,32 @@
 // Service ID: service_wtl0a9d
 // UID: hLSWPmSsE780AMibL
 
-// note we can also use async/await
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${
+      y * boolInt
+    }px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += "dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -28,7 +53,6 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false;
